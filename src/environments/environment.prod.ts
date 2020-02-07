66f26708df1bc5d2358a70e8aeb8b37769ebc9ee
Pavanaditya Mylavarapu <M1050962@mindtree.com>
2020-02-08 00:31:35 +0530

    api key changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseURL: `https://www.googleapis.com/youtube/v3/`,
-  apiKey: 'api-key-123'
-  // apiKey: `AIzaSyC3PT_WKdlngPACw49H4rPWJgHChAMExU4`
-  // apiKey: `AIzaSyBl04MoiM3BbaWmLxL_CifhZleV9UUBgZU`
+  // apiKey: 'api-key-123'
+  // apiKey: `AIzaSyC3PT_WKdlngPACw49H4rPWJgHChAMExU49`
+  apiKey: `AIzaSyBl04MoiM3BbaWmLxL_CifhZleV9UUBgZU`
 };
